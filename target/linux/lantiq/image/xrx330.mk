53bf1a4fd21ceeec048d87a55357c79a3f52379f
root <root@localhost>
2025-06-13 23:09:22 +0200

    soc should match device tree file name

--- target/linux/lantiq/image/xrx330.mk
+++ target/linux/lantiq/image/xrx330.mk
@@ -2,7 +2,7 @@ define Device/draytek_vigor2862
   $(Device/NAND)
   DEVICE_VENDOR := Draytek
   DEVICE_MODEL := Vigor2862
-  SOC := grx390
+  SOC := xrx330
   DEVICE_PACKAGES := kmod-usb-dwc2 u-boot-tools
 endef
 TARGET_DEVICES += draytek_vigor2862
